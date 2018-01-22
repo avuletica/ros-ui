05734cf63a26273a043800d7a778177b067c99ba
Gligorije Cupkovic <gcupko00@fesb.hr>
2018-01-22 17:40:38 +0100

    Websocket connection fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,54 +14,34 @@ export class AppComponent {
 
     const ros = new ROSLIB.Ros({url: 'ws://localhost:9090'});
 
-    // Create a connection to the rosbridge WebSocket server.
-    ros.connect('ws://localhost:9090');
-
-    // Publishing a Topic
-    // ------------------
-
-    // First, we create a Topic object with details of the topic's name and message type.
-    const cmdVel = new ROSLIB.Topic({
-      ros: ros,
-      name: '/cmd_vel',
-      messageType: 'geometry_msgs/Twist'
+    // If there is an error on the backend, an 'error' emit will be emitted.
+    ros.on('error', function(error) {
+      console.log(error);
     });
 
-    // Then we create the payload to be published. The object we pass in to ros.Message matches the
-    // fields defined in the geometry_msgs/Twist.msg definition.
-    const twist = new ROSLIB.Message({
-      linear: {
-        x: 0.1,
-        y: 0.2,
-        z: 0.3
-      },
-      angular: {
-        x: -0.1,
-        y: -0.2,
-        z: -0.3
-      }
+    // Find out exactly when we made a connection.
+    ros.on('connection', function() {
+      console.log('Connection made!');
+
+      // If we made a connection, subscribe to velocity topic
+      const listener = new ROSLIB.Topic({
+        ros: ros,
+        name: '/mobile_base/commands/velocity',
+        messageType: 'geometry_msgs/Twist'
+      });
+  
+      listener.subscribe(function (message) {
+        // Print out the velocity to the console
+        console.log('Received linear speed message on ' + listener.name + ': ' + message.linear.x);
+  
+        // If desired, we can unsubscribe from the topic as well.
+        // listener.unsubscribe();
+      });
+
     });
 
-    // And finally, publish.
-    cmdVel.publish(twist);
-
-    // Subscribing to a Topic
-    // ----------------------
-
-    // Like when publishing a topic, we first create a Topic object with details of the topic's name
-    // and message type. Note that we can call publish or subscribe on the same topic object.
-    const listener = new ROSLIB.Topic({
-      ros: ros,
-      name: '/listener',
-      messageType: 'std_msgs/String'
-    });
-
-    // Then we add a callback to be called every time a message is published on this topic.
-    listener.subscribe(function (message) {
-      console.log('Received message on ' + listener.name + ': ' + message);
-
-      // If desired, we can unsubscribe from the topic as well.
-      listener.unsubscribe();
+    ros.on('close', function() {
+      console.log('Connection closed.');
     });
   }
 }
