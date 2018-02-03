c8368976caaee98a35083e67e74572fcfe8c2e24
Ante Vuletić Antić <vuletic23@gmail.com>
2018-02-03 11:35:17 +0100

    display velocity & angular data, remove angular logo & add custom power button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RosService} from './services/RosService';
 
 @Component({
@@ -6,16 +6,36 @@ import {RosService} from './services/RosService';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  velocityLinearY: number;
+  velocityLinearX: number;
+  velocityLinearZ: number;
+  velocityAngularX: number;
+  velocityAngularY: number;
+  velocityAngularZ: number;
 
   constructor(private ros: RosService) {
 
   }
 
-  test(): void {
+  ngOnInit(): void {
+    this.velocityLinearX = 0;
+    this.velocityLinearY = 0;
+    this.velocityLinearZ = 0;
+    this.velocityAngularX = 0;
+    this.velocityAngularY = 0;
+    this.velocityAngularZ = 0;
+  }
+
+  startRos(): void {
     this.ros.getVelocityObservable().subscribe(velocity => {
-      console.log(velocity.linear.y, velocity.linear.x);
+      this.velocityLinearX = velocity.linear.x;
+      this.velocityLinearY = velocity.linear.y;
+      this.velocityLinearZ = velocity.linear.z;
+
+      this.velocityAngularX = velocity.angular.x;
+      this.velocityAngularY = velocity.angular.y;
+      this.velocityAngularZ = velocity.angular.z;
     });
   }
 }
